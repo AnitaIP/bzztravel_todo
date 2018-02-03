cd1c6702f9263af5b2076e11ba97cc6d8b7021f5
Anita Špakauskė <you@example.com>
2018-02-03 15:55:29 +0200

    fix

---
+++ app/src/main/java/lt/bzztravel/todo/todoapp/Model/ItemVO.java
@@ -0,0 +1,11 @@
+package lt.bzztravel.todo.todoapp.Model;
+
+/**
+ * Created by Anita on 2018-02-03.
+ */
+
+public class ItemVO {
+    public int id;
+    public int done;
+    public String title;
+}
